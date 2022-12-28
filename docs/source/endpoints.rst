213c59b67d372f707fb94290b6de5c0b03279345
Garrett Nelson <garrett3nelson@gmail.com>
2022-12-27 22:29:54 -0400

    Adding documentation to OSRS endpoints

--- docs/source/endpoints.rst
+++ docs/source/endpoints.rst
@@ -7,3 +7,31 @@ WikiQuery
 This is the parent class for all queries to the various endpoints.
 
 .. autoclass:: rswiki_wrapper.wiki.WikiQuery
+
+Real-Time API
+---------------
+
+This is the parent class for all queries to the Real Time API for OSRS. It is recommended to use one of the child classes documented below for standard usage.
+
+.. autoclass:: rswiki_wrapper.osrs.RealTimeQuery
+
+Latest
+------
+
+.. autoclass:: rswiki_wrapper.osrs.Latest
+
+Mapping
+-------
+
+.. autoclass:: rswiki_wrapper.osrs.Mapping
+
+Average Price
+-------------
+
+.. autoclass:: rswiki_wrapper.osrs.AvgPrice
+
+Time Series
+-----------
+
+.. autoclass:: rswiki_wrapper.osrs.TimeSeries
+
--- rswiki_wrapper/osrs.py
+++ rswiki_wrapper/osrs.py
@@ -6,6 +6,25 @@ from collections import OrderedDict
 
 
 class RealTimeQuery(WikiQuery):
+    """
+    A class for making real-time price queries to the RuneScape Wiki API.
+    
+    This class extends the `WikiQuery` class and provides specific functionality for making queries to the real-time price endpoints of the RuneScape Wiki API. The class provides options for querying the latest prices, mapping item IDs to names, and requesting time-series data for specific items.
+    
+    :param route: The specific route to query within the endpoint. Can be one of 'latest', 'mapping', 'price-query', or 'time-series'.
+    :type route: str, optional
+    :param endpoint: The specific endpoint to query. Can be one of 'osrs', 'dmm', or 'fsw'.
+    :type endpoint: str, optional
+    :param user_agent: The user agent string to include in the HTTP request headers.
+    :type user_agent: str, optional
+    :param \**kwargs: Additional keyword arguments to include in the query parameters. Valid arguments depend on the route and endpoint being queried.
+    :type \**kwargs: Any, optional
+    
+    .. attribute:: json
+       :type: dict
+    
+        The raw JSON response from the API.
+    """
     def __init__(self, route="", endpoint="osrs", user_agent='RS Wiki API Python Wrapper - Default', **kwargs):
         # Valid endpoints are 'osrs', 'dmm', and 'fsw'
         # https://oldschool.runescape.wiki/w/RuneScape:Real-time_Prices for full documentation
@@ -24,38 +43,118 @@ class RealTimeQuery(WikiQuery):
 
 
 class Latest(RealTimeQuery):
-    def __init__(self, user_agent='RS Wiki API Python Wrapper - Default', **kwargs):
-        super().__init__(route="latest", user_agent=user_agent, **kwargs)
+    """
+    A class for querying the latest real-time prices from the RuneScape Wiki API.
+    
+    This class extends the `RealTimeQuery` class and provides specific functionality for making queries to the 'latest' route of the real-time price endpoints of the RuneScape Wiki API. The class provides options for querying the latest prices for specific items.
+    
+    :param user_agent: The user agent string to include in the HTTP request headers.
+    :type user_agent: str, optional
+    :param game: The game type to provide prices. Valid games 'osrs', 'dmm', and 'fsw'. Default OSRS
+    :type game: str, optional
+    :param \**kwargs: For this endpoint, the optional keyword is 'id' and the value is the itemID to query.
+    :type \**kwargs: str, optional
+
+    .. note:: It is best practice to query all item ids (do not provide a kwarg) and to loop through the content for specific IDs you require. This results in one query instead of multiple if you provide single ids.
+    
+    .. attribute:: content
+       :type: dict
+    
+        The real-time prices for the items specified in the query.
+        
+    """
+    def __init__(self, game='osrs', user_agent='RS Wiki API Python Wrapper - Default', **kwargs):
+        super().__init__(route="latest", endpoint=game, user_agent=user_agent, **kwargs)
 
         # Response is {'data': {OrderedDict()}}
         self.content = self.json['data']
 
 
 class Mapping(RealTimeQuery):
-    def __init__(self, user_agent='RS Wiki API Python Wrapper - Default', **kwargs):
-        super().__init__(route="mapping", user_agent=user_agent)
+    """
+    A class for querying the item mappings from the RuneScape Wiki API.
+    
+    This class extends the `RealTimeQuery` class and provides specific functionality for making queries to the 'mapping' route of the real-time price endpoints of the RuneScape Wiki API. The class provides options for querying the item mappings available in the API.
+    
+    :param game: The game type to provide prices. Valid games 'osrs', 'dmm', and 'fsw'. Default OSRS
+    :type game: str, optional
+    :param user_agent: The user agent string to include in the HTTP request headers.
+    :type user_agent: str, optional
+    
+    .. attribute:: content
+       :type: list
+    
+        The item mappings available in the API.
+    
+    .. note:: No additional keyword arguments are required for this class.
+    
+    """
+    def __init__(self, game='osrs', user_agent='RS Wiki API Python Wrapper - Default'):
+        super().__init__(route="mapping", endpoint=game, user_agent=user_agent)
 
         # Response is [OrderedDict()]
         self.content = self.json
 
 
 class AvgPrice(RealTimeQuery):
-    def __init__(self, route, user_agent='RS Wiki API Python Wrapper - Default', **kwargs):
+    """
+    A class for querying the average real-time prices from the RuneScape Wiki API.
+
+    This class extends the `RealTimeQuery` class and provides specific functionality for making queries to the '5m' or '1h' routes of the real-time price endpoints of the RuneScape Wiki API. The class provides options for querying the average prices for specific items at different time intervals.
+
+    :param route: The route to query. Must be '5m' or '1h'.
+    :type route: str
+    :param game: The game type to provide prices. Valid games 'osrs', 'dmm', and 'fsw'. Default OSRS
+    :type game: str, optional
+    :param user_agent: The user agent string to include in the HTTP request headers.
+    :type user_agent: str, optional
+    :param \**kwargs: For both endpoints, 'timestamp' is the keyword and the value is the UNIX formatted timestamp to begin the average.
+    :type \**kwargs: str, optional
+
+    .. attribute:: content
+       :type: dict
+
+        The average real-time prices for items.
+
+    """
+    def __init__(self, route, game='osrs', user_agent='RS Wiki API Python Wrapper - Default', **kwargs):
         # Valid routes are '5m' or '1h'
 
         # TODO Validate the timestamp is valid if the kwarg is used
         # TODO Validate the route is valid (5m, 1h)
-        super().__init__(route, user_agent=user_agent, **kwargs)
+        super().__init__(route, endpoint=game, user_agent=user_agent, **kwargs)
 
         # Response is {'data': {OrderedDict()}}
         self.content = self.json['data']
 
 
 class TimeSeries(RealTimeQuery):
-    def __init__(self, user_agent='RS Wiki API Python Wrapper - Default', **kwargs):
+    """
+    A class for querying the time-series real-time prices from the RuneScape Wiki API.
+
+    This class extends the `RealTimeQuery` class and provides specific functionality for making queries to the 'timeseries' route of the real-time price endpoints of the RuneScape Wiki API. The class provides options for querying the time-series data for specific items.
+
+    :param game: The game type to provide prices. Valid games 'osrs', 'dmm', and 'fsw'. Default OSRS
+    :type game: str, optional
+    :param user_agent: The user agent string to include in the HTTP request headers.
+    :type user_agent: str, optional
+    :param \**kwargs: Additional keyword arguments to include in the query parameters. See note below for description of required kwargs
+    :type \**kwargs: str, required
+
+    .. attribute:: content
+       :type: list
+
+        The time-series real-time prices for the item specified in the query.
+
+    .. note:: Keyword arguments are required as follows:
+
+       - Required keyword argument 'id' with a value of the item ID for which to retrieve time-series data.
+       - Required keyword argument 'timestep' with a value of the period of the time-series data to retrieve. Valid values are '5m', '1h', or '6h'.
+
+    """
+    def __init__(self, game='osrs', user_agent='RS Wiki API Python Wrapper - Default', **kwargs):
         # TODO Validate the timestep is valid (5m, 1h, 6h)
-        super().__init__(route="timeseries", user_agent=user_agent, **kwargs)
+        super().__init__(route="timeseries", endpoint=game, user_agent=user_agent, **kwargs)
 
         # Response is {'data': [{OrderedDict()}]}
         self.content = self.json['data']
-
